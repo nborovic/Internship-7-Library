a4aacd0851569d59ed902394b1db38d9adf23be2
nborovic <nino.borovic@outlook.com>
2019-02-08 23:15:24 +0100

    Project setup #1

---
+++ Library.Presentation/Library.Data/Entities/Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Library.Data.Entities.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Book Book { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
---
+++ Library/Library.Data/Entities/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Entities.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [MaxLength(30)] public string FirstName { get; set; }
+        [MaxLength(30)] public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        [MaxLength(3)] public string Class { get; set; }
+    }
+}
